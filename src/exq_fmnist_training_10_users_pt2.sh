76325491315ed21680e647081ca7fd17f240f92a
Pil Kyu Cho <comsci.p.cho@gmail.com>
2024-06-17 13:19:22 +0200

    update scripts

--- src/exq_fmnist_training_10_users_pt2.sh
+++ src/exq_fmnist_training_10_users_pt2.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
-# federated with dirchlet 0.3
-python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=0.3 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
-python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=0.3 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
-python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=0.3 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
-python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=0.3 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
-python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=0.3 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
 
-# federated with dirchlet 0.5
-python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=0.5 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
-python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=0.5 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
-python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=0.5 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
-python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=0.5 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
-python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=0.5 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
+# federated with dirchlet 1.0
+python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=1.0 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
+python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=1.0 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
+python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=1.0 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
+python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=1.0 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
+python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=1.0 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
+
+# federated with dirchlet 10.0
+python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=10.0 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
+python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=10.0 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
+python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=10.0 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
+python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=10.0 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
+python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=10.0 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
+
+# federated with dirchlet 100.0
+python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=100.0 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
+python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=100.0 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
+python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=100.0 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
+python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=100.0 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
+python federated_main.py --model=exq --dataset=fmnist --gpu=cuda:0 --epochs=20 --iid=2 --dirichlet=100.0 --num_users=10 --frac=1.0 --local_ep=2 --local_bs=32 --lr=0.001 --unequal=0 --num_generate=0
